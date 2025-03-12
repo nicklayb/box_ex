2995d345c486cc4ef58455de2e753f6def01e5bd
nboisvert <nicklay@me.com>
2025-03-11 21:58:32 -0400

    Test

--- .github/actions/tag_repo/index.js
+++ .github/actions/tag_repo/index.js
@@ -8,6 +8,13 @@ try {
   readFile(VERSION_FILE, { encoding: "utf8" }).then(content => {
     const version = content.trim()
 
+    github.getOctokit().rest.repos.getReleaseByTag({
+      ...github.context,
+      tag_sha: version
+    }).then(result => {
+      console.log(result)
+    })
+
     console.log(version)
 
     core.setOutput('version', version)
