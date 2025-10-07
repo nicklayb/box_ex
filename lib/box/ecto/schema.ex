db226cf9e8a5e4f18eeb0e479c304c205a6dbfd8
nicklayb <nicklay@me.com>
2025-10-07 14:12:04 -0400

    Fix credo

--- lib/box/ecto/schema.ex
+++ lib/box/ecto/schema.ex
@@ -2,6 +2,7 @@ if Code.ensure_loaded?(Ecto.Schema) do
   defmodule Box.Ecto.Schema do
     defmacro flag(setter, options \\ []) do
       quote do
+        # credo:disable-for-lines:3
         field(:"#{unquote(setter)}", :boolean, virtual: true)
         setter_field = Keyword.get(unquote(options), :key, "#{unquote(setter)}_at")
         field(:"#{setter_field}", :naive_datetime)
