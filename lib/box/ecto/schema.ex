1735adc593eda2c1cc5ec6873c9de45f6d611b24
nicklayb <nicklay@me.com>
2025-10-07 14:20:21 -0400

    Fix type

--- lib/box/ecto/schema.ex
+++ lib/box/ecto/schema.ex
@@ -5,7 +5,7 @@ if Code.ensure_loaded?(Ecto.Schema) do
         # credo:disable-for-lines:3
         field(:"#{unquote(setter)}", :boolean, virtual: true)
         setter_field = Keyword.get(unquote(options), :key, "#{unquote(setter)}_at")
-        field(:"#{setter_field}", :naive_datetime)
+        field(:"#{setter_field}", :utc_datetime)
       end
     end
   end
