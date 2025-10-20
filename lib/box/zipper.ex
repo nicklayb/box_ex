5c1993b2ba52fd1cd5444764b89407d5c09df0f6
nicklayb <nicklay@me.com>
2025-10-20 11:27:28 -0400

    Add zipper

---
+++ lib/box/zipper.ex
@@ -0,0 +1,73 @@
+defmodule Box.Zipper do
+  @moduledoc """
+  Data structure that zips over a single data
+  """
+  defstruct previous: [], next: [], current: nil
+
+  alias Box.Zipper
+
+  @type t :: %Zipper{previous: list(), next: list(), current: any()}
+
+  defguardp is_cont_or_reset(cont_or_reset) when cont_or_reset in ~w(cont reset)a
+
+  @spec new(nonempty_list()) :: t()
+  def new([current | next]) do
+    %Zipper{next: next, current: current}
+  end
+
+  def new([]), do: raise(ArgumentError, message: "List must not be empty")
+
+  @type move_result :: {:cont, t()} | {:reset, t()}
+
+  @spec new(t() | move_result()) :: move_result()
+  def next(%Zipper{previous: previous, current: current, next: []}) do
+    [current | next] = Enum.reverse([current | previous])
+    {:reset, %Zipper{next: next, previous: [], current: current}}
+  end
+
+  def next(%Zipper{previous: previous, current: current, next: [new_current | next]}) do
+    {:cont, %Zipper{previous: [current | previous], current: new_current, next: next}}
+  end
+
+  def next({cont_or_reset, %Zipper{} = zipper}) when is_cont_or_reset(cont_or_reset) do
+    next(zipper)
+  end
+
+  @spec previous(t() | move_result()) :: move_result()
+  def previous(%Zipper{previous: [], current: current, next: next}) do
+    [current | previous] = Enum.reverse([current | next])
+
+    {:cont, %Zipper{previous: previous, current: current, next: []}}
+  end
+
+  def previous(%Zipper{previous: [new_current | previous], current: current, next: next}) do
+    case %Zipper{previous: previous, current: new_current, next: [current | next]} do
+      %Zipper{previous: []} = zipper -> {:reset, zipper}
+      %Zipper{} = zipper -> {:cont, zipper}
+    end
+  end
+
+  def previous({cont_or_reset, %Zipper{} = zipper}) when is_cont_or_reset(cont_or_reset) do
+    previous(zipper)
+  end
+
+  @spec current(t()) :: any()
+  def current(%Zipper{current: current}) do
+    current
+  end
+
+  @spec reset(t()) :: t()
+  def reset(%Zipper{previous: []} = zipper) do
+    zipper
+  end
+
+  def reset(zipper) do
+    case next(zipper) do
+      {:cont, new_zipper} ->
+        reset(new_zipper)
+
+      {:reset, new_zipper} ->
+        new_zipper
+    end
+  end
+end
---
+++ test/box/zipper_test.exs
@@ -0,0 +1,83 @@
+defmodule Box.ZipperTest do
+  use Box.BaseCase
+
+  alias Box.Zipper
+
+  describe "new/1" do
+    test "create a zipper" do
+      assert %Zipper{
+        previous: [],
+        current: :a,
+        next: [:b, :c, :d]
+      }
+
+      assert_raise(ArgumentError, fn -> Zipper.new([]) end)
+    end
+  end
+
+  describe "next/1" do
+    test "pick next item and circle back once finished" do
+      initial_zipper = Zipper.new([1, 2, 3])
+      assert %Zipper{previous: [], current: 1, next: [2, 3]} = initial_zipper
+
+      assert {:cont, %Zipper{previous: [1], current: 2, next: [3]} = zipper} =
+               Zipper.next(initial_zipper)
+
+      assert {:cont, %Zipper{previous: [2, 1], current: 3, next: []} = zipper} =
+               Zipper.next(zipper)
+
+      assert {:reset, ^initial_zipper} =
+               Zipper.next(zipper)
+
+      assert {:cont, %Zipper{}} =
+               initial_zipper
+               |> Zipper.next()
+               |> Zipper.next()
+    end
+  end
+
+  describe "previous/1" do
+    test "pick next item and circle back once finished" do
+      initial_zipper = Zipper.new([1, 2, 3])
+      assert %Zipper{previous: [], current: 1, next: [2, 3]} = initial_zipper
+
+      assert {:cont, %Zipper{previous: [2, 1], current: 3, next: []} = zipper} =
+               Zipper.previous(initial_zipper)
+
+      assert {:cont, %Zipper{previous: [1], current: 2, next: [3]} = zipper} =
+               Zipper.previous(zipper)
+
+      assert {:reset, ^initial_zipper} =
+               Zipper.previous(zipper)
+
+      assert {:cont, %Zipper{}} =
+               initial_zipper
+               |> Zipper.previous()
+               |> Zipper.previous()
+    end
+  end
+
+  describe "reset/1" do
+    test "resets a zipper" do
+      initial_zipper = Zipper.new([1, 2, 3])
+
+      assert initial_zipper == Zipper.reset(initial_zipper)
+
+      assert {:cont, next_zipper} = Zipper.next(initial_zipper)
+
+      refute next_zipper == initial_zipper
+
+      assert initial_zipper == Zipper.reset(next_zipper)
+    end
+  end
+
+  describe "current/1" do
+    test "gets current element" do
+      initial_zipper = Zipper.new([1, 2, 3])
+      assert 1 == Zipper.current(initial_zipper)
+
+      assert {:cont, zipper} = Zipper.next(initial_zipper)
+      assert 2 == Zipper.current(zipper)
+    end
+  end
+end
